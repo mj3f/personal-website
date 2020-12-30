e29079bb9fdc869e796cc591a593bbd852fa689e
Mike Pratt <mike.pratt@trelleborg.com>
2020-12-30 23:25:33 +0000

    Added font awesome icons to footer. Added favicon and emoji image to project. Changed site title.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,11 @@
 import React, { FunctionComponent } from 'react';
+import memoji from './img/memoji.png';
 
 const Home: FunctionComponent = () => (
-    <p className="text-center">Hello world!</p>
+    <div className="flex flex-col flex-wrap w-full h-full justify-center items-center">
+        <img src={memoji} alt="memoji me" className="h-48 w-48 flex-auto" />
+        <p className="flex-auto">Hello there!</p>
+    </div>
 );
 
 export default Home;
