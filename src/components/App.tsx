0a25f1f3e358842942da8f01c234be31fe63b713
Mike Pratt <mike.pratt@trelleborg.com>
2020-12-31 15:37:46 +0000

    Refactored project - moved components into components directory. Added linked in and copyright links to footer. Added dark/light mode icons next to toggle switch. Other styling tweaks.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,19 @@ import Home from './Home';
 import Projects from './Projects';
 import ReactSwitch from 'react-switch';
 import Footer from './Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
     const toggle = () => setIsDarkMode(prevState => !prevState);
 
-    const navLinkClass = 'uppercase p-2 text-gray-800 dark:text-white';
+    const navLinkClass = 'uppercase p-2 text-gray-800 dark:text-white hover:text-green-500';
     const navLinkActiveClass = 'border-b-2 border-green-500';
     const marginRight2 = 'mr-2';
+
+    const darkModeIcon = <FontAwesomeIcon icon={faMoon} />
+    const lightModeIcon = <FontAwesomeIcon icon={faSun} />
 
     return (
         <BrowserRouter>
@@ -24,8 +29,11 @@ const App = () => {
                                 <NavLink to="/projects" className={navLinkClass} exact activeClassName={navLinkActiveClass}>Projects</NavLink>
                             </div>
                             <div className="w-1/2 flex justify-end">
-                                <div className="p-2">
-                                    <ReactSwitch checked={isDarkMode} onChange={toggle} />
+                                <div className="h-full p-2 flex flex-col justify-center">
+                                    <ReactSwitch checked={isDarkMode} onChange={toggle} onColor="#10B981" checkedIcon={false} uncheckedIcon={false} />
+                                </div>
+                                <div className="h-full p-2 flex flex-col justify-center">
+                                    {isDarkMode ? darkModeIcon : lightModeIcon}
                                 </div>
                             </div>
                         </nav>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import memoji from './img/memoji.png';
+import memoji from '../img/memoji.png';
 
 const Home: FunctionComponent = () => (
     <div className="flex flex-col flex-wrap w-full h-full justify-center items-center">
