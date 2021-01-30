c51565753d5e1f18cec4e88b202b8fb11dc10ea3
Mike Pratt <mike.pratt@trelleborg.com>
2021-01-30 20:56:01 +0000

    Tried to fix fade animation but it's impossible, gg

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,26 +4,37 @@ import memoji from '../img/memoji.png';
 const Home: FunctionComponent = () => {
     const [state, setState] = useState({
         index: 0,
-        aboutMeSnippet: 'make things'
+        aboutMeSnippet: 'make things.'
     });
+
+    const [fade, setFade] = useState(false);
 
     useEffect(() => {
         const aboutStrings = [
-            'make things',
-            'break stuff',
-            'like to learn new technologies',
-            'EXECUTE ORDER 66',
-            'am an F1 fanatic',
-            'think Yu-Gi-Oh! > Pokémon',
-            'peddle around on two wheels',
-            '<3 movies',
+            'make things.',
+            'break stuff.',
+            'like to learn new technologies.',
+            'EXECUTE ORDER 66.',
+            'think Yu-Gi-Oh! > Pokémon.',
+            'peddle around on two wheels.',
+            '<3 movies.',
+            'have 💎 👐',
             'think you\'re alright!'
         ];
-        const index = state.index === aboutStrings.length - 1 ? 0 : state.index + 1;
-        setTimeout(() => setState({
-            aboutMeSnippet: aboutStrings[index],
-            index: index
-        }), 3000);
+
+        const func = async() => {
+            await setFade(false);
+            const index = state.index === aboutStrings.length - 1 ? 0 : state.index + 1;
+            setTimeout(() => {
+                setState({
+                    aboutMeSnippet: aboutStrings[index],
+                    index: index,
+                });
+            }, 3000);
+            await setFade(true);
+        };
+
+        func();
     }, [state])
 
     // FIXME add animate-fade class to state.aboutMeSnippet, there is a time slippage between setTimeout and the animation duration of 3 seconds.
@@ -32,8 +43,8 @@ const Home: FunctionComponent = () => {
         <div className="flex flex-col flex-wrap w-full h-full justify-center items-center">
             <img src={memoji} alt="My mind is blown, always!" className="h-48 w-48 flex-auto"/>
             <div className="flex flex-auto w-full justify-center">
-                <div className="w-1/2 text-right text-2xl font-semibold">Hello there! I'm Mike and I </div>
-                <div className="w-1/2 pl-1 text-2xl font-semibold">{state.aboutMeSnippet}.</div>
+                {console.log(fade)}
+                <div className={fade ? "fade text-2xl font-semibold" : "text-2xl font-semibold"}>Hello there! I'm Mike and I {state.aboutMeSnippet}</div>
             </div>
             <div className="flex-auto pt-5">
                 I am currently a <a className="p-0 font-semibold">Software Engineer</a> at {' '}
