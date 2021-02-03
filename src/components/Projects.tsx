35243c560d9453cb01d562be51301ee277d0ede7
Mike Pratt <mike.pratt@trelleborg.com>
2021-02-03 18:17:38 +0000

    Fixed link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 
 const Projects: FunctionComponent = () => (
     <div className="text-center">
-        <a href="fpl.msjp.dev">Fantasy Premier League API wrapper written in .NET Core.</a>
+        <a href="https://www.fpl.msjp.dev" className="hover:text-green-500">Fantasy Premier League API wrapper written in .NET Core.</a>
     </div>
 );
 
