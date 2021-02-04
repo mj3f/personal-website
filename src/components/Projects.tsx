3b40bff7ea50180bfb2adc14bf6f10b0038858f5
Mike Pratt <mike.pratt@trelleborg.com>
2021-02-04 16:17:38 +0000

    Added projects heading to projects component.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent } from 'react';
 
 const Projects: FunctionComponent = () => (
     <div className="text-center">
-        <a href="https://fpl.msjp.dev/" className="hover:text-green-500">Fantasy Premier League API wrapper written in .NET Core.</a>
+        <h1 className="text-3xl font-semibold p-5">Projects</h1>
+        <a href="https://fpl.msjp.dev/" className="hover:text-green-500">Fantasy Premier League API wrapper written in .NET Core (unfinished).</a>
     </div>
 );
 
