1d8a19a2deefc2ab94aced00b3530e5b56cbb6db
Mike Pratt <mike@msjp.me>
2025-08-13 08:40:50 +0100

    chore: updates to site for 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,21 +11,31 @@ const Footer: React.FC = () => {
   return (
     <div className="flex flex-col items-center p-2 md:flex-row md:justify-between">
       <div className="flex w-full justify-center md:justify-start">
-        <p className="dark:text-white">© 2022-2024 Mike Jeffs</p>
+        <p className="dark:text-white">© 2022-2025 Mike Jeffs</p>
       </div>
       <div className="flex w-full justify-center">
         <Link href="https://github.com/mj3f/" className={anchorClass}>
           <FontAwesomeIcon icon={faGithub} size={"2x"} />
         </Link>
-        <Link href="https://www.linkedin.com/in/michael-jeffs-pratt-650046123/" className={anchorClass}>
+        <Link
+          href="https://www.linkedin.com/in/michael-jeffs-pratt-650046123/"
+          className={anchorClass}
+        >
           <FontAwesomeIcon icon={faLinkedin} size={"2x"} />
         </Link>
-        <Link href="mailto:mikep2319@me.com" aria-label="email" className={anchorClass}>
+        <Link
+          href="mailto:mikep2319@me.com"
+          aria-label="email"
+          className={anchorClass}
+        >
           <FontAwesomeIcon icon={faEnvelope} size={"2x"} />
         </Link>
       </div>
       <div className="flex w-full justify-center md:justify-end">
-        <Link href="https://github.com/mj3f/personal-website" className="font-light tex-sm dark:text-white">
+        <Link
+          href="https://github.com/mj3f/personal-website"
+          className="font-light tex-sm dark:text-white"
+        >
           source code
         </Link>
       </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,60 +2,67 @@ import ContentPageLayout from "../components/ContentPageLayout";
 import Layout from "../components/Layout";
 
 export default function About() {
-    return (
-        <Layout>
-            <ContentPageLayout title="Hey, I'm Mike" showHandWave>
-               <div className="flex w-full pt-5">
-                    <article className="flex flex-col w-full md:w-2/3">
-                        <p className="p-2">I'm a Software Engineer with over 7 years of professional experience, specialising in 
-                            building modern, responsive and performant web applications with Angular/React and .NET Core.
-                            I'm currently working as a Software Engineer at Keoghs.
-                        </p>
-                        <p className="p-2">
-                            I love learning new things,
-                            from creating beautiful web UIs and building fast & secure APIs, to containerizing all the things
-                            and hacking serial ports. 
-                        </p>
-                        {/* NOT READY YET: <p>I have started a technical blog where I will be documenting my learnings, you can find it <a href="">here</a>.</p> */}
-                        <p className="p-2">I started programming in 2012. In 2014 I enrolled at the University of Liverpool to study Computer Science,
-                            graduating in 2017 with a first.
-                        </p>
-                        <p className="p-2">In my spare time you can find me chilling out with my 2 cats, reading books, playing video games, 
-                            at the gym, watching movies and occasionally jumping on a trampoline.
-                        </p>
-                    </article>
-                    <div className="hidden md:w-1/3 md:flex md:justify-end">
-                        <div className="h-96 w-96 opacity-75 bg-amber-300 rounded-full flex items-center justify-center">
-                            <p className="opacity-0">Nothing to see here!</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col pt-5 w-full">
-                    <p className="text-2xl md:text-4xl pl-2 pb-2 font-semibold text-center">Technical Skills</p>
-                    <div className="flex justify-evenly">
-                        <ul className="list-disc columns-1 md:columns-3">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>CSS Frameworks (MUI, Tailwind)</li>
-                            <li>JavaScript/TypeScript</li>
-                            <li>Angular (2+)</li>
-                            <li>React</li>
-                            <li>C# .NET Web APIs & Azure Function Apps</li>
-                            <li>SQL Server/PostgreSQL</li>
-                            <li>Cosmos DB</li>
-                            <li>Git</li>
-                            <li>Linux</li>
-                            <li>Docker</li>
-                            <li>Azure</li>
-                            <li>CI/CD using Github actions, Azure DevOps</li>
-                            <li>Terraform</li>
-                            <li className="opacity-50">Android (Java)</li>
-                            <li className="opacity-50">iOS (Swift)</li>
-                            <li className="opacity-50">Laravel</li>
-                        </ul>
-                    </div>
-                </div>
-            </ContentPageLayout>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <ContentPageLayout title="Hey, I'm Mike" showHandWave>
+        <div className="flex w-full pt-5">
+          <article className="flex flex-col w-full md:w-2/3">
+            <p className="p-2">
+              I'm a Software Engineer with over 8 years of professional
+              experience, specialising in building modern, responsive and
+              performant web applications with Angular/React and .NET Core. I'm
+              currently working as a Software Engineer at Keoghs.
+            </p>
+            <p className="p-2">
+              I love learning new things, from creating beautiful web UIs and
+              building fast & secure APIs, to containerizing all the things and
+              hacking serial ports.
+            </p>
+            {/* NOT READY YET: <p>I have started a technical blog where I will be documenting my learnings, you can find it <a href="">here</a>.</p> */}
+            <p className="p-2">
+              I started programming in 2012. In 2014 I enrolled at the
+              University of Liverpool to study Computer Science, graduating in
+              2017 with a first.
+            </p>
+            <p className="p-2">
+              In my spare time you can find me chilling out with my 2 cats,
+              reading books, playing video games, at the gym, watching movies
+              and getting thrown around in Judo.
+            </p>
+          </article>
+          <div className="hidden md:w-1/3 md:flex md:justify-end">
+            <div className="h-96 w-96 opacity-75 bg-amber-300 rounded-full flex items-center justify-center">
+              <p className="opacity-0">Nothing to see here!</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col pt-5 w-full">
+          <p className="text-2xl md:text-4xl pl-2 pb-2 font-semibold text-center">
+            Technical Skills
+          </p>
+          <div className="flex justify-evenly">
+            <ul className="list-disc columns-1 md:columns-3">
+              <li>HTML</li>
+              <li>CSS (+ frameworks like MUI, Tailwind)</li>
+              <li>TypeScript</li>
+              <li>React</li>
+              <li>Angular (2+)</li>
+              <li>C#</li>
+              <li>.NET & Azure</li>
+              <li>SQL Server/PostgreSQL</li>
+              <li>Cosmos DB</li>
+              <li>Git</li>
+              <li>Linux</li>
+              <li>Docker</li>
+              <li>Azure</li>
+              <li>CI/CD using Github actions, Azure DevOps</li>
+              <li>Terraform</li>
+              <li className="opacity-50">Android (Java)</li>
+              <li className="opacity-50">iOS (Swift)</li>
+            </ul>
+          </div>
+        </div>
+      </ContentPageLayout>
+    </Layout>
+  );
 }
